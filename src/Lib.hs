da62fdfe5570605cd9000bf4f6d37f651b4c99f6
Hans Montero <hjm2133@columbia.edu>
2020-12-05 23:32:02 -0500

    Implement rough COM traversal

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,22 +3,22 @@ module Lib where
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-data Body = Body {  mass :: Double
-                 ,  xCord :: Double
-                 ,  yCord :: Double
-                 ,  velocity :: Double
+data Body = Body {  mass :: Integer
+                 ,  xCord :: Integer
+                 ,  yCord :: Integer
+                 ,  velocity :: Integer
                  }
 
-data CenterMass = CenterMass {  cMass :: Double
-                             ,  cx :: Double
-                             ,  cy :: Double
+data CenterMass = CenterMass {  cMass :: Integer
+                             ,  cx :: Integer
+                             ,  cy :: Integer
                              }
 
-data QuadInfo = QuadInfo {  xl :: Double
-                         ,  xr :: Double
-                         ,  yt :: Double
-                         ,  yb :: Double
-                         ,  com :: Maybe CenterMass
+data QuadInfo = QuadInfo {  xl :: Integer
+                         ,  xr :: Integer
+                         ,  yt :: Integer
+                         ,  yb :: Integer
+                         ,  com :: CenterMass
                          }
 
 instance Show Body where
@@ -31,13 +31,13 @@ getInfo :: QuadTree -> QuadInfo
 getInfo (QuadTree _ _ _ _ qi) = qi
 getInfo (QuadNode _ qi) = qi
 
-emptyQNode :: Double -> Double -> Double -> Double -> QuadTree
-emptyQNode xl xr yt yb = QuadNode Nothing (QuadInfo xl xr yt yb Nothing)
+emptyQNode :: Integer -> Integer -> Integer -> Integer -> QuadTree
+emptyQNode xl xr yt yb = QuadNode Nothing (QuadInfo xl xr yt yb (CenterMass 0 0 0))
 
-emptyQTree :: Double -> Double -> Double -> Double -> QuadTree
-emptyQTree xl xr yt yb = QuadTree nw ne sw se (QuadInfo xl xr yt yb Nothing)
-                        where xm = (xr - xl) / 2
-                              ym = (yt - yb) / 2
+emptyQTree :: Integer -> Integer -> Integer -> Integer -> QuadTree
+emptyQTree xl xr yt yb = QuadTree nw ne sw se (QuadInfo xl xr yt yb (CenterMass 0 0 0))
+                        where xm = (xr - xl) `div` 2
+                              ym = (yt - yb) `div` 2
                               nw = emptyQNode xl xm yt ym
                               ne = emptyQNode xm xr yt ym
                               sw = emptyQNode xl xm ym yb
@@ -53,11 +53,30 @@ insert b (QuadTree nw ne sw se qi)
   | inQuad se b = insert b sw
   | otherwise = error "Couldn't find QuadTree to insert body"
 
-calculateMass :: QuadTree -> QuadTree
-calculateMass (QuadNode Nothing qi) = QuadNode Nothing qi
-calculateMass (QuadNode (Just b) qi) = QuadNode (Just b) (qi {com = Just $ CenterMass (mass b) (xCord b) (yCord b)})
-calculateMass (QuadTree nw ne sw se qi) = QuadTree nw ne sw se (qi {com = })
-
+calculateCOM :: QuadTree -> QuadTree
+calculateCOM (QuadNode Nothing qi) = QuadNode Nothing qi
+calculateCOM (QuadNode (Just b) qi) = QuadNode (Just b) (qi {com = CenterMass (mass b) (xCord b) (yCord b)})
+calculateCOM (QuadTree nw ne sw se qi) = QuadTree nw ne sw se (qi {com = CenterMass newMass newX newY})
+                where nwCOM = com $ getInfo nw
+                      nwMass = cMass nwCOM
+                      nwX = cx nwCOM
+                      nwY = cy nwCOM
+                      neCOM = com $ getInfo ne 
+                      neMass = cMass neCOM
+                      neX = cx neCOM
+                      neY = cy neCOM
+                      swCOM = com $ getInfo sw
+                      swMass = cMass swCOM
+                      swX = cx swCOM
+                      swY = cy swCOM
+                      seCOM = com $ getInfo se 
+                      seMass = cMass seCOM
+                      seX = cx seCOM
+                      seY = cy seCOM
+                      newMass = nwMass + neMass + swMass + seMass
+                      newX = ((nwMass * nwX) + (neMass * neX) + (swMass * swX) + (seMass * seX)) `div` newMass
+                      newY = ((nwMass * nwY) + (neMass * neY) + (swMass * swY) + (seMass * seY)) `div` newMass
+                      
 inQuad :: QuadTree -> Body -> Bool
 inQuad qt b = xl qi <= x && xr qi >= x && yt qi >= y && yb qi <= y
             where x = xCord b
