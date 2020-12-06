dc09d67281576da884be806d8108b1254f6903be
Rhys Murray <rhysmurray@Rhyss-MacBook-Pro.local>
2020-12-05 22:51:27 -0500

    Started calculateMass and finished insert

--- src/Lib.hs
+++ src/Lib.hs
@@ -43,8 +43,20 @@ emptyQTree xl xr yt yb = QuadTree nw ne sw se (QuadInfo xl xr yt yb Nothing)
                               sw = emptyQNode xl xm ym yb
                               se = emptyQNode xm xr ym yb
 
-insert :: QuadTree -> Body -> QuadTree
-insert (QuadNode Nothing qi) b = QuadNode 
+insert :: Body -> QuadTree -> QuadTree
+insert b (QuadNode Nothing qi) = QuadNode (Just b) qi
+insert b2 (QuadNode (Just b1) qi) = insert b2 $ insert b1 $ emptyQTree (xl qi) (xr qi) (yt qi) (yb qi)
+insert b (QuadTree nw ne sw se qi)
+  | inQuad nw b = insert b nw
+  | inQuad ne b = insert b ne
+  | inQuad sw b = insert b se
+  | inQuad se b = insert b sw
+  | otherwise = error "Couldn't find QuadTree to insert body"
+
+calculateMass :: QuadTree -> QuadTree
+calculateMass (QuadNode Nothing qi) = QuadNode Nothing qi
+calculateMass (QuadNode (Just b) qi) = QuadNode (Just b) (qi {com = Just $ CenterMass (mass b) (xCord b) (yCord b)})
+calculateMass (QuadTree nw ne sw se qi) = QuadTree nw ne sw se (qi {com = })
 
 inQuad :: QuadTree -> Body -> Bool
 inQuad qt b = xl qi <= x && xr qi >= x && yt qi >= y && yb qi <= y
