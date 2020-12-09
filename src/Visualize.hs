e773ea513ea22fd7893c5da4036dff1c19c11ead
Hans Montero <hjm2133@columbia.edu>
2020-12-09 01:04:03 -0500

    Add debug

--- src/Visualize.hs
+++ src/Visualize.hs
@@ -9,16 +9,16 @@ getCircle size = circle size
 
 drawBody :: Body -> Picture
 drawBody b = Translate x y (getCircle mass')
-  where x = realToFrac $ xCord b
-        y = realToFrac $ yCord b
-        mass' = realToFrac $ mass b
+    where x = realToFrac $ xCord b
+          y = realToFrac $ yCord b
+          mass' = realToFrac $ mass b
 
 drawQuadTree :: QuadTree -> Picture
 drawQuadTree = pictures . map drawBody . toList
 
 runSimulation :: QuadTree -> (QuadTree -> Double -> QuadTree) -> IO ()
-runSimulation qt updateFunc = simulate (InWindow "Nice Window" (1500, 1500) (10, 10)) 
-			       white 60 
-			       qt
-			       (\(qt) -> drawQuadTree qt)  
-			       (\_ dt (qt) -> updateFunc qt (float2Double dt))
+runSimulation qt updateFunc = simulate (InWindow "Barnes-Hut Simulation" (1500, 1500) (10, 10)) 
+                              white 60
+                              qt
+                              (\(qt) -> drawQuadTree qt)
+                              (\_ dt (qt) -> updateFunc qt (float2Double dt))
