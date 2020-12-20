43f2a7ff7d91c1a0f4593054f9f6a6288693be6f
Rhys Murray <ram2269@columbia.edu>
2020-12-20 18:29:21 -0500

    Fixed non-parallel test

--- test/test.py
+++ test/test.py
@@ -101,13 +101,13 @@ def other_test():
 
 		if strategy == 'default':
 
-			continue
 			for bodies in body_count:
 				run_time = run_sim(iterations, bodies, 1) # no parallel
 				clock_time = run_time[0]
 				strategyResults[strategy] = clock_time
-				results[strategy] = strategyResults
-				continue
+			
+			results[strategy] = strategyResults
+			continue
 
 		for core in cores:
 
